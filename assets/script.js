e2e0f5ec95828dcc31f66e91dc16c7b4b41f27d9
marzec <gregmarzec02@gmail.com>
2022-07-01 20:54:09 -0400

    added time change? and updated id's

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,6 @@
+var displayDay = $("#currentDay");
 var time = moment();
+var day = moment().format("MMMM Do, YYYY")
 
 var row1 = $("#row1");
 var row2 = $("#row2");
@@ -22,8 +24,10 @@ var input9 = $("#input9");
 
 var box9 = $("#9ambox")
 
+displayDay.text(day);
+//before hour 9
 if (time.format("H") > 9) {
-    input1.css("background-color","lightgreen");
+    input1.css("background-color","lightgray");
     input2.css("background-color","lightgreen");
     input3.css("background-color","lightgreen");
     input4.css("background-color","lightgreen");
@@ -32,4 +36,112 @@ if (time.format("H") > 9) {
     input7.css("background-color","lightgreen");
     input8.css("background-color","lightgreen");
     input9.css("background-color","lightgreen");
+}
+//hour 9
+if (time.format("H") = 9) {
+    input1.css("background-color","lightred");
+    input2.css("background-color","lightgreen");
+    input3.css("background-color","lightgreen");
+    input4.css("background-color","lightgreen");
+    input5.css("background-color","lightgreen");
+    input6.css("background-color","lightgreen");
+    input7.css("background-color","lightgreen");
+    input8.css("background-color","lightgreen");
+    input9.css("background-color","lightgreen");
+}
+//hour 10
+if (time.format("H") = 10) {
+    input1.css("background-color","lightgray");
+    input2.css("background-color","lightred");
+    input3.css("background-color","lightgreen");
+    input4.css("background-color","lightgreen");
+    input5.css("background-color","lightgreen");
+    input6.css("background-color","lightgreen");
+    input7.css("background-color","lightgreen");
+    input8.css("background-color","lightgreen");
+    input9.css("background-color","lightgreen");
+}
+//hour 11
+if (time.format("H") = 11) {
+    input1.css("background-color","lightgray");
+    input2.css("background-color","lightgray");
+    input3.css("background-color","lightred");
+    input4.css("background-color","lightgreen");
+    input5.css("background-color","lightgreen");
+    input6.css("background-color","lightgreen");
+    input7.css("background-color","lightgreen");
+    input8.css("background-color","lightgreen");
+    input9.css("background-color","lightgreen");
+}
+//hour 12pm
+if (time.format("H") = 12) {
+    input1.css("background-color","lightgray");
+    input2.css("background-color","lightgray");
+    input3.css("background-color","lightgray");
+    input4.css("background-color","lightred");
+    input5.css("background-color","lightgreen");
+    input6.css("background-color","lightgreen");
+    input7.css("background-color","lightgreen");
+    input8.css("background-color","lightgreen");
+    input9.css("background-color","lightgreen");
+}
+//hour 1pm
+if (time.format("H") = 13) {
+    input1.css("background-color","lightgray");
+    input2.css("background-color","lightgray");
+    input3.css("background-color","lightgray");
+    input4.css("background-color","lightgray");
+    input5.css("background-color","lightred");
+    input6.css("background-color","lightgreen");
+    input7.css("background-color","lightgreen");
+    input8.css("background-color","lightgreen");
+    input9.css("background-color","lightgreen");
+}
+//hour 2pm
+if (time.format("H") = 14) {
+    input1.css("background-color","lightgray");
+    input2.css("background-color","lightgray");
+    input3.css("background-color","lightgray");
+    input4.css("background-color","lightgray");
+    input5.css("background-color","lightgray");
+    input6.css("background-color","lightred");
+    input7.css("background-color","lightgreen");
+    input8.css("background-color","lightgreen");
+    input9.css("background-color","lightgreen");
+}
+//hour 3pm
+if (time.format("H") = 15) {
+    input1.css("background-color","lightgray");
+    input2.css("background-color","lightgray");
+    input3.css("background-color","lightgray");
+    input4.css("background-color","lightgray");
+    input5.css("background-color","lightgray");
+    input6.css("background-color","lightgray");
+    input7.css("background-color","lightred");
+    input8.css("background-color","lightgreen");
+    input9.css("background-color","lightgreen");
+}
+//hour 4pm
+if (time.format("H") = 16) {
+    input1.css("background-color","lightgray");
+    input2.css("background-color","lightgray");
+    input3.css("background-color","lightgray");
+    input4.css("background-color","lightgray");
+    input5.css("background-color","lightgray");
+    input6.css("background-color","lightgray");
+    input7.css("background-color","lightgray");
+    input8.css("background-color","lightred");
+    input9.css("background-color","lightgreen");
+}
+//hour 5pm
+if (time.format("H") = 17) {
+    input1.css("background-color","lightgray");
+    input2.css("background-color","lightgray");
+    input3.css("background-color","lightgray");
+    input4.css("background-color","lightgray");
+    input5.css("background-color","lightgray");
+    input6.css("background-color","lightgray");
+    input7.css("background-color","lightgray");
+    input8.css("background-color","lightgray");
+    input9.css("background-color","lightred");
 }
